08c8362903cd027d8e0a7b1d8523c42460e300c8
Ben <bengoehring1@gmail.com>
2020-01-22 06:55:41 -0500

    boop

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,11 +1,11 @@
 # Cauchy
 CauchyData <- load("data-raw/CauchyData.Rda")
-save(CauchyData, file = "data/CauchyData.Rda", version = 2)
+save(CauchyData, file = "data/CauchyData.Rda")
 
 # CarStop
 Pset1CarStopData <- load("data-raw/Pset1CarStopData.Rda")
-save(Pset1CarStopData, file = "data/Pset1CarStopData.Rda", version = 2)
+save(Pset1CarStopData, file = "data/Pset1CarStopData.Rda")
 
 # Voter List
 `voterList-AgeRace` <- load("data-raw/voterList-AgeRace.Rda")
-save(`voterList.subset`, file = "data/voterList-AgeRace.Rda", version = 2)
+save(`voterList.subset`, file = "data/voterList-AgeRace.Rda")
