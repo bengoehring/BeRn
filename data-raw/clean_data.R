2b243fd0a15f418686323f1caf7b967e68506f59
Ben <bengoehring1@gmail.com>
2020-01-22 06:33:12 -0500

    another try

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,11 +1,11 @@
 # Cauchy
-CauchyData <- load("data/CauchyData.Rda")
+CauchyData <- load("data-raw/CauchyData.Rda")
 save(CauchyData, file = "data/CauchyData.Rda", version = 2)
 
 # CarStop
-Pset1CarStopData <- load("data/Pset1CarStopData.Rda")
+Pset1CarStopData <- load("data-raw/Pset1CarStopData.Rda")
 save(Pset1CarStopData, file = "data/Pset1CarStopData.Rda", version = 2)
 
 # Voter List
-`voterList-AgeRace` <- load("data/voterList-AgeRace.Rda")
+`voterList-AgeRace` <- load("data-raw/voterList-AgeRace.Rda")
 save(`voterList.subset`, file = "data/voterList-AgeRace.Rda", version = 2)
