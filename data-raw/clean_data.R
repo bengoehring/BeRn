70e9520d9244f998125906e715b468fc51731b8d
Ben <bengoehring1@gmail.com>
2020-01-22 07:03:40 -0500

    maybe this time

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -1,11 +1,11 @@
 # Cauchy
 CauchyData <- load("data-raw/CauchyData.Rda")
-save(CauchyData, file = "data/CauchyData.Rda")
+usethis::use_data(CauchyData, overwrite = TRUE)
 
 # CarStop
 Pset1CarStopData <- load("data-raw/Pset1CarStopData.Rda")
-save(Pset1CarStopData, file = "data/Pset1CarStopData.Rda")
+usethis::use_data(Pset1CarStopData, overwrite = TRUE)
 
 # Voter List
 `voterList-AgeRace` <- load("data-raw/voterList-AgeRace.Rda")
-save(`voterList.subset`, file = "data/voterList-AgeRace.Rda")
+usethis::use_data(`voterList-AgeRace`, overwrite = TRUE)
