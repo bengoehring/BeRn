17a78aa93261f03c43f942c13f6575613269ac77
Ben Goehring <bengoehring1@gmail.com>
2025-06-09 16:28:21 -0500

    colors try 1

---
+++ .Rproj.user/C3B99AF3/sources/session-4dc6160/0C64737B-contents
@@ -0,0 +1,9 @@
+#' um_colors
+#'
+#' A vector containing the University of Michigan's official colors
+#'
+#' @format A character vector of length 15.
+#'
+#' @examples
+#' um_colors
+"um_colors"
---
+++ R/uva_colors.R
@@ -0,0 +1,9 @@
+#' uva_colors
+#'
+#' A vector containing the University of Virginia's official colors.
+#'
+#' @format A character vector of length 14.
+#'
+#' @examples
+#' uva_colors
+"uva_colors"
